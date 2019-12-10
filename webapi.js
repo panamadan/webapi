50fc9a8ff683420f98e652513ddf30660f6de306
Juan De La Guardia <delaguardia@outlook.com>
2019-12-10 18:42:35 -0500

    second page

--- webapi.js
+++ webapi.js
@@ -1,2 +1,31 @@
-var questions = document.getElementById("questions")
-var 
+
+var btn = document.getElementById("start-button")
+btn.onclick = function startGame() {
+    $( "#startCard" ).hide()
+    $("#allQuestions").removeClass("invisible");
+    var choicesArea = $("#choices");
+ 
+    var currentQuestion = questions[0]
+    $("#questions").text(currentQuestion.title);
+ for (let i = 0; i < currentQuestion.choices.length; i++) {
+     var choiceBtn = $("<button>");
+     choiceBtn.text(currentQuestion.choices[i]);
+     choicesArea.append(choiceBtn);
+     
+ }
+
+}
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    
+    // add more
+  ];
